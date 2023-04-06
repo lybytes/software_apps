273e304859b4a76564aeb969e49f43d01bd1c9e3
lybytes <124044060+lybytes@users.noreply.github.com>
2023-04-06 10:16:59 +0100

    Update chess_stats_analysis.py

--- chess_stats_analysis.py
+++ chess_stats_analysis.py
@@ -5,50 +5,65 @@ from scipy.stats import norm # For fitting
 from scipy.stats import rayleigh
 
 class ChessPlayer:
-    def __init__(self, name, yob, gender, federation):
+    def __init__(self, name, yob, gender, federation, id):
         self.yob = yob
         self.federation = federation
         self.gender = gender
         self.name = name
+        self.id = id
         #self.fide_id = fide_id
 
-players_data = []
+idToPlayers = {}
 yob_data = []
-elo = {}
-elos = []
+elo_data = []
 # Open the CSV file
+print('We made it out lol')
 with open('players.csv', 'r') as file:
 
     # Create a CSV reader object
     reader = csv.DictReader(file)
+    #print(reader.fieldnames)
 
     # Create an empty list to store the data
-    
     for row in reader:
     
         yob = int(row['yob']) if row['yob'] else None
         federation = row['federation']
         gender = row['gender']
-        #fide_id = int(row['fide_id']) if row['fide_id'] else None
+        id = row['\ufefffide_id']
         name = row['name']
-        new_player = ChessPlayer(name, yob, gender, federation) #fide_id
-        players_data.append(new_player)
-        yob_data.append(yob)
-    
+        new_player = ChessPlayer(name, yob, gender, federation, id) #fide_id
+        player_info = []
+        player_info.append(new_player)
+        
+        idToPlayers[id] = player_info
+     
 
 with open('ratings_2021_condensed.csv', 'r') as file:
     
     reader = csv.DictReader(file)
-    count = 0
+    #print(reader.fieldnames)
     for row in reader:
+        id = row['fide_id']
         elo_2021 = row['rating_standard']
-        elo[players_data[count]] = elo_2021
-        elos.append(elo_2021)
-        count += 1
-        
-plt.plot(yob_data, elos)
+        if id in idToPlayers:
+            idToPlayers[id].append(elo_2021)
+
+yob_data = []
+elo_data = []
+for player_info in idToPlayers.values():
+    if len(player_info) > 1:
+        player = player_info[0]
+        elo = player_info[1] # Assuming elo is an integer
+        if player.yob is not None and elo is not None and player.yob >= 1950 and player.yob <= 2025:
+            yob_data.append(player.yob)
+            elo_data.append(elo)
+
+# Create the plot with customized x-axis
+plt.scatter(yob_data, elo_data)
+plt.xlim(1950, 2023)
+plt.xlabel('Year of Birth')
+plt.ylabel('Elo Rating')
+plt.title('Elo Rating vs. Year of Birth')
 plt.show()
-
-
-
-    
+            
