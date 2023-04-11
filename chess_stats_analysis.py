89b69c84d479d00d341a00014053ff24b7c7d7aa
lybytes <124044060+lybytes@users.noreply.github.com>
2023-04-11 22:41:13 +0100

    Update chess_stats_analysis.py
    Completed code for Q1, added outlines for other questions

--- chess_stats_analysis.py
+++ chess_stats_analysis.py
@@ -13,19 +13,14 @@ class ChessPlayer:
         self.gender = gender
         self.name = name
         self.id = id
-        #self.fide_id = fide_id
 
 idToPlayers = {}
 yob_data = []
 elo_data = []
-# Open the CSV file
 with open('players.csv', 'r') as file:
 
-    # Create a CSV reader object
     reader = csv.DictReader(file)
-    #print(reader.fieldnames)
 
-    # Create an empty list to store the data
     for row in reader:
     
         yob = int(row['yob']) if row['yob'] else None
@@ -39,69 +34,67 @@ with open('players.csv', 'r') as file:
         
         idToPlayers[id] = player_info
      
+     
+     
+     
 
-
-
+#Q1 - What is the relationship between age and chess elo? Here, we are plotting every player's elo value against their year of birth
+#     and seeing if there is correlation
+all_yob_to_elo = []
 with open('ratings_2021_condensed.csv', 'r') as file:
     
     reader = csv.DictReader(file)
-    #print(reader.fieldnames)
     for row in reader:
         id = row['fide_id']
-        elo_2021 = row['rating_standard']
+        elo_2021 = int(row['rating_standard']) if row['rating_standard'] else None
         if id in idToPlayers:
             idToPlayers[id].append(elo_2021)
+            yob_to_elo = []
+            yob_to_elo.append(idToPlayers[id][0].yob)
+            yob_to_elo.append(idToPlayers[id][1])
+            all_yob_to_elo.append(yob_to_elo)
+            
 
-yob_data = []
-elo_data = []
-count = 0
-for player_info in idToPlayers.values():
-    if len(player_info) > 1 and count < 50:
-        player = player_info[0]
-        elo = player_info[1] # Assuming elo is an integer
-        if player.yob is not None and elo is not None and player.yob >= 1950 and player.yob <= 2025:
-            yob_data.append(player.yob)
-            elo_data.append(elo)
-            count += 1
+all_yob_to_elo = [yob_elo for yob_elo in all_yob_to_elo if yob_elo[1] is not None]
+sorted(all_yob_to_elo, key=lambda x: x[0])
 
+x = [item[0] for item in all_yob_to_elo]
+y = [item[1] for item in all_yob_to_elo]
+# Filtering out elements in x that are less than 1950
+x_filtered = [year for year in x if year >= 1950]
+y_filtered = [y[i] for i in range(len(x)) if x[i] >= 1950]
 
+# Find the minimum and maximum year of birth in the data and using that as axis scales
+min_yob = min(x_filtered)
+max_yob = max(x_filtered)
+yob_range= range(min_yob, max_yob+1, 10)
 
-plt.scatter(yob_data, elo_data)
+slope, intercept = np.polyfit(x_filtered, y_filtered, 1)
+line_x = [min_yob, max_yob]
+line_y = [slope * xi + intercept for xi in line_x]
+
+plt.scatter(x_filtered, y_filtered , s = 1)
+plt.plot(line_x, line_y, color='r')
 plt.xlabel('Year of Birth')
 plt.ylabel('Elo Rating')
 plt.title('Elo Rating vs. Year of Birth')
+plt.yticks(np.arange(800, 2900, 200))
+plt.xticks(yob_range)
 plt.show()
+#It would seem that, naturally, there is a negative relationship between a player's year of birth and their chess elo rating, 
 
 
 
-
-"""
-
-elo_data_reformatted = np.array(elo_data)
-elo_data_reformatted2 = elo_data_reformatted.astype(float)
-print('Max of elo data', np.amax(elo_data_reformatted2))    # Some descriptive statistics 
-print('Min of elo data', np.amin(elo_data_reformatted2))
-print('Mean of elo', np.mean(elo_data_reformatted2))
-print('Std. dev. of elo', np.std(elo_data_reformatted2))
+#Q2. How big a difference is there between the top 100 rated male players, and the top 100 rated female players?
 
 
 
-plt.scatter(sample_yob_data, sample_elo_data)
-plt.xlim(1950, 2023)
-plt.ylim(500, 2500)
-plt.xlabel('Year of Birth')
-plt.ylabel('Elo Rating')
-plt.title('Elo Rating vs. Year of Birth')
-plt.show()
-
-elo_data_reformatted = np.array(elo_data)
-elo_data_reformatted2 = elo_data_reformatted.astype(float)
-print('Max of elo data', np.amax(elo_data_reformatted2))    # Some descriptive statistics 
-print('Min of elo data', np.amin(elo_data_reformatted2))
-print('Mean of elo', np.mean(elo_data_reformatted2))
-print('Std. dev. of elo', np.std(elo_data_reformatted2))
+#Q3. How does the elo rating of top players overtime change (in this particular experiment, from 2015 to 2021)
 
 
-plt.xlim(1950, 2023)
-plt.ylim(500, 2500)
-"""
+#Q4. How much does the average elo rating vary according to federations? Would players from, for example, the Russian federation, 
+#    have a higher average rating than players from the Irish federation? How much?
+
+
+#Q5. Is there a relationship between a person's IQ and their chess elo rating?
+
